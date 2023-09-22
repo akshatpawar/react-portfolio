ee501ba3e8c8cd4e452f6cb53bf1a1c1ed2130a1
akshat <akshatpawar1@gmail.com>
2023-09-22 19:19:21 +0530

    update

---
+++ .react-email/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,24 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\app\\page": [
+      "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\styles\\globals.css",
+      "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ],
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\app\\preview\\[slug]\\page": [
+      "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\styles\\globals.css",
+      "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\app\\page.tsx": [
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\styles\\globals.css"
+  ],
+  "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\app\\layout.tsx": [
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\styles\\globals.css"
+  ],
+  "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\app\\preview\\[slug]\\page.tsx": [
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\Code\\HTML\\New P\\new-react-portfolio\\.react-email\\src\\styles\\globals.css"
+  ]
+}
